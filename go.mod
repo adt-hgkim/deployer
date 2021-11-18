fe58a33d80e9c03ab512cd9b3225b18bf667a1df
adt-hgkim <hgkim@gnmpm.com>
2021-11-18 14:34:30 +0900

    ✨ cors allows all origins

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/adt-hgkim/deployer
 go 1.16
 
 require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.4
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"os"
 )
@@ -17,6 +18,7 @@ func main() {
 	}
 
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "deployer",
